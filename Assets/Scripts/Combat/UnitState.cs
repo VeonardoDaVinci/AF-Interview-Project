ec32ef1482555cfdd8e6b954e48e44361a87b628
Nikodem Kazmierczak <nikodeminio12@gmail.com>
2024-02-16 16:20:36 +0100

    Add tweening plugin for easy procedural animation.

--- Assets/Scripts/Combat/UnitState.cs
+++ Assets/Scripts/Combat/UnitState.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 namespace AFSInterview
 {
     public abstract class UnitState
@@ -27,13 +26,7 @@ namespace AFSInterview
         private int CalculateAttackDamage()
         {
             int attack = 0;
-            if (Unit.UnitData.AttackDamageOverride.Attribute.Equals(UnitAttribute.None))
-            {
-                attack = Unit.UnitData.AttackDamage - attackTarget.UnitData.ArmorPoints;
-                Debug.Log("Attack made by " + Unit.name + " on " + attackTarget.name + " for " + attack);
-                return attack > 1 ? attack : 1;
-            }
-            if (attackTarget.UnitData.Attribute.HasFlag(Unit.UnitData.AttackDamageOverride.Attribute))
+            if (attackTarget.UnitData.Attribute.HasFlag(Unit.UnitData.AttackDamageOverride.Attribute) && !Unit.UnitData.AttackDamageOverride.Attribute.Equals(UnitAttribute.None))
             {
                 attack = Unit.UnitData.AttackDamageOverride.AttackDamage - attackTarget.UnitData.ArmorPoints;
                 Debug.Log("Attack made by " + Unit.name + " on " + attackTarget.name + " for " + attack);
